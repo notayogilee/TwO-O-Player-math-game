6912780d3a0abde672c189310e279cd27430a8ac
Lee <notayogilee@gmail.com>
2020-04-27 19:21:20 +0000

    add spacing

--- game.rb
+++ game.rb
@@ -3,18 +3,23 @@ require_relative './player'
 class Game
   
   def initialize
+
     @player1 = Player.new("Player 1")
     @player2 = Player.new("Player 2")
     @current_player = @player2
+
   end
 
   def alternate_player
+
     @current_player == @player1 ?
     @current_player = @player2 : 
     @current_player = @player1  
+
   end
 
   def math_question(player)
+
     x = rand(1..20)
     y = rand(1..20)
     result = x + y
@@ -31,6 +36,7 @@ class Game
 
   end
 
+
   def turn(player)
   
     print "#{player.name}: " 
@@ -38,17 +44,20 @@ class Game
     
   end
 
+
   def no_lives_left?
 
     @current_player.lives < 1
 
   end
 
+
   def round_stats
 
     puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
 
   end
+
 
   def game_over(player)
 
@@ -58,6 +67,7 @@ class Game
 
   end
 
+  
   def play
     
     until(no_lives_left?) do
